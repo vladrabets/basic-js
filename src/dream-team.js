4364b4b17a544c3925eb30f24af4ef1cfbeef7c5
Rabets Vlad <vladrabets46@gmai.com>
2021-02-02 14:45:35 +0300

    what season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members))
+    return false;
+
+  let team='';
+  members=members.filter(str => typeof(str)==="string")
+  members=members.map(function(str){
+    str = str.split(' ').join('');
+    return str;
+  }
+  );
+  team = members.map(item => item.split('')[0]);
+  team = team.join('').toUpperCase().split('').sort().join('');
+
+  if(team==='')
+    return false;
+  else 
+    return team;
+
 };
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(date==undefined)
+    return 'Unable to determine the time of year!';
+  try{
+    date.toJSON()
+  }
+  catch(err){
+    throw new Error(err)
+}
+let season = date.getMonth();
+if(season>=2 && season<=4)
+        return 'spring';
+     else if(season>=5 && season<=7)
+            return 'summer';
+          else if(season>=8 && season<=10)
+                  return 'autumn';
+               else return 'winter';
 };
