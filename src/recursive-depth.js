884bdadcd9e7bb89ae1fea12c101bb7c8060b67b
Vlad Rabets <vladrabets46@gmail.com>
2021-02-11 13:01:49 +0300

    2 ex

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxDepth = 1;
+    for(let item of arr){
+      if(Array.isArray(item)){
+        let depth = this.calculateDepth(item)+1;
+				if(depth > maxDepth) maxDepth = depth;
+      }
+    }
+    return maxDepth;
   }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: '',
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(this.chain.length!=0)
+    this.chain+="~~";
+    this.chain+=`( ${value} )`;
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position<1 || typeof(position)!='number')
+      {
+        this.chain='';
+        throw new Error;
+      }
+    else
+    {
+      this.chain=this.chain.split('~~');
+      this.chain.splice(position-1,1);
+      this.chain=this.chain.join('~~');
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(this.chain.length>1)
+      this.chain=this.chain.split('~~').reverse().join('~~');
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let newChain = this.chain;
+    this.chain='';
+    return newChain;
   }
 };
 
